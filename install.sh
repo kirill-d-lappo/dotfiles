4915506f16c0e3e612fb4bbfe14d5f826c6ff9d2
Kirill Lappo <kirill-lappo@outlook.com>
2025-01-21 23:30:01 +0300

    Update install.sh
    * git configuration added

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 tdir="$HOME/.dotfiles/"
 wtree="$HOME"
 
+echo "Installing dotfiles...."
+
 echo ".dotfiles" >> "$HOME/.gitignore"
 
 git clone --bare "https://github.com/kirill-d-lappo/dotfiles.git" "$tdir"
@@ -12,5 +14,31 @@ alias config='/usr/bin/git --git-dir="$tdir" --work-tree=$wtree'
 git --git-dir="$tdir" --work-tree="$wtree" config --local status.showUntrackedFiles no
 git --git-dir="$tdir" --work-tree="$wtree" checkout -f
 
-echo "Restart you session"
 
+echo "Configuring git ...."
+
+git config --global alias.amend "commit --amend --no-editw"
+git config --global alias.lol "log --pretty=format:'%x09%x09 %Cred%h%Creset -%Creset %<(60,trunc)%s%Cgreen%<(13,trunc)(%cr) %C(bold blue)%<(15,trunc)<%an>%Creset %C(yellow)%d%Creset' --abbrev-commit --graph"
+
+git config --global core.editor "nvim -n --clean"
+
+git config --global core.pager  "delta"
+git config --global interactive.diffFilter "delta --color-only"
+
+git config --global delta.syntax-theme "Dracula"
+git config --global delta.line-numbers "true"
+git config --global delta.side-by-side "false"
+git config --global delta.navigate "true"
+git config --global delta.light "false"
+
+git config --global merge.conflictstyle diff3
+git config --global diff.colorMoved default
+
+git config --global fetch.prune true
+git config --global fetch.pruneTags true
+
+git config --global push.autoSetupRemote true
+git config --global init.defaultBranch main
+
+
+echo "Restart you session"
