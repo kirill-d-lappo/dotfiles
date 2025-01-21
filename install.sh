9d9499af687b635c9c765e20f385f7821c409e42
Kirill Lappo <kirill.lappo@digitech7.by>
2025-01-21 16:08:09 +0300

    script update

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
-
-
 tdir = "$HOME/.dotfiles/"
-mkdir -p "$tdir"
 
 echo ".dotfiles" >> "$HOME/.gitignore"
 
 git clone --bare https://github.com/kirill-d-lappo/dotfiles.git $tdir
+
 alias config='/usr/bin/git --git-dir=$tdir --work-tree=$HOME'
 
 config config --local status.showUntrackedFiles no
