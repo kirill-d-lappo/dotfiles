ca541221a19014ca204f27abcbad92ee559144ff
Kirill Lappo <kirill.lappo@digitech7.by>
2025-01-21 16:05:18 +0300

    better script

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 
 
+tdir = "$HOME/.dotfiles/"
+mkdir -p "$tdir"
 
-alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-echo ".dotfiles" >> .gitignore
+echo ".dotfiles" >> "$HOME/.gitignore"
 
-git clone --bare https://github.com/kirill-d-lappo/dotfiles.git $HOME/.dotfiles/
+git clone --bare https://github.com/kirill-d-lappo/dotfiles.git $tdir
+alias config='/usr/bin/git --git-dir=$tdir --work-tree=$HOME'
 
 config config --local status.showUntrackedFiles no
+
+config checkout -f
 
 echo "Restart you session"
 # echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.bashrc
