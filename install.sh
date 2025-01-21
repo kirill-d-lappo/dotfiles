565dda7efb946ea2daddb675599ec56d50e66afa
Kirill Lappo <kirill.lappo@digitech7.by>
2025-01-21 18:05:56 +0300

    script fix

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ git clone --bare "https://github.com/kirill-d-lappo/dotfiles.git" "$tdir"
 
 alias config='/usr/bin/git --git-dir="$tdir" --work-tree=$wtree'
 
-git --git-dir="$tdir" --work-tree="$wtree" --work-tree config --local status.showUntrackedFiles no
-git --git-dir="$tdir" --work-tree="$wtree" --work-tree checkout -f
+git --git-dir="$tdir" --work-tree="$wtree" config --local status.showUntrackedFiles no
+git --git-dir="$tdir" --work-tree="$wtree" checkout -f
 
 echo "Restart you session"
 
