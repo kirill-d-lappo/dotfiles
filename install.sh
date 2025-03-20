88ecc63c63c80466799e4215d709926b4c04890e
Kirill Lappo <kirill-lappo@outlook.com>
2025-03-20 13:59:19 +0300

    Update install.sh: added git-deltat to install list

--- install.sh
+++ install.sh
@@ -133,7 +133,7 @@ git config --global init.defaultBranch main
 
 # install cargo tools
 
-cargo_tools=("bat" "zoxide" "starship" "eza" "fd-find" "alacritty" "viu")
+cargo_tools=("bat" "zoxide" "starship" "eza" "fd-find" "alacritty" "viu" "git-delta")
 if has_command cargo; then
   echo "Installing helper tools using cargo"
 
