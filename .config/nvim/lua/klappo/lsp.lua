58b3df78e66563e08f2415bed5480eafd131895d
Kirill Lappo <kirill-lappo@outlook.com>
2025-11-20 18:43:24 +0300

    nvim config update

--- .config/nvim/lua/klappo/lsp.lua
+++ .config/nvim/lua/klappo/lsp.lua
@@ -1,5 +1,5 @@
-local config_lsp = vim.lsp.config;
-local enable_lsp = vim.lsp.enable;
+local config_lsp = vim.lsp.config
+local enable_lsp = vim.lsp.enable
 
 local utils = require("klappo.utils")
 
@@ -9,16 +9,16 @@ config_lsp("powershell_es", {
   root_markers = {
     ".git",
     ".luarc.json",
-    ".luarc.jsonc"
+    ".luarc.jsonc",
   },
   diagnostics = {
     globals = { "vim" },
-  }
-});
+  },
+})
 
 config_lsp("powershell_es", {
   bundle_path = utils.get_mason_package_folder_path("powershell-editor-services"),
-});
+})
 
 config_lsp("omnisharp", {
   cmd = { "dotnet", utils.get_mason_package_folder_path("omnisharp") .. "/libexec/OmniSharp.dll" },
@@ -27,9 +27,9 @@ config_lsp("omnisharp", {
   organize_imports_on_format = true,
   enable_decompilation_support = true,
   filetypes = { "cs", "vb", "csproj", "sln", "slnx", "props", "csx", "targets" },
-});
+})
 
-enable_lsp("lua_ls");
-enable_lsp("ts_ls");
-enable_lsp("powershell_es");
-enable_lsp("omnisharp");
+enable_lsp("lua_ls")
+enable_lsp("ts_ls")
+enable_lsp("powershell_es")
+enable_lsp("omnisharp")
